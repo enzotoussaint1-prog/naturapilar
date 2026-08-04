b85dc79482361be41e40913ea65430601656bd7a
enzotoussaint1-prog <enzotoussaint1@gmail.com>
2026-08-04 18:26:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,58 +1,6 @@
-const productos = [
-    {
-        id: 1,
-        nombre: "Kit Tododia Algodón",
-        descripcion: "Crema hidratante 400 ml + Body Splash 200 ml",
-        categoria: "Tododia",
-        subcategoria:"Cremas",
-        precio: 31900,
-        imagen: "img/algodon.jpeg",
-        oferta: true,
-        destacado: true,
-        stock: 8
-    },
-    {
-        id: 2,
-        nombre: "Kit Tododia Acerola",
-        descripcion: "Crema hidratante + Body Splash",
-        categoria: "Tododia",
-        subcategoria:"Cremas",
-        precio: 31900,
-        imagen: "img/acerola.jpeg",
-        oferta: true,
-        destacado: true,
-        stock: 8
-    },
-    {
-        id: 3,
-        nombre: "Pitanga Preta",
-        descripcion: "Perfume Ekos 150 ml",
-        categoria: "Perfumes",
-        subcategoria:"Ekos",
-        precio: 30000,
-        imagen: "img/pitanga.jpeg",
-        oferta: true,
-        destacado: true,
-        stock: 8
-    },
-    {
-        id: 4,
-        nombre: "Kit Humor Rojo",
-        descripcion: "Labial + Esmalte + Perfume",
-        categoria: "Maquillaje",
-        subcategoria:"Humor",
-        precio: 20000,
-        imagen: "img/humor.png",
-        oferta: true,
-        destacado: true,
-        stock: 8
-    }
-];
 const contenedorProductos = document.getElementById("contenedor-productos");
-console.log(contenedorProductos);
 
 function renderizarProductos(lista) {
-     console.log(lista);
 
     contenedorProductos.innerHTML = "";
 
